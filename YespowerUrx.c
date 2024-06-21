b4136f5c5f2591226d96356e15ad77737f67d298
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2024-06-21 09:56:15 -0500

    Update YespowerUrx.c

--- YespowerUrx.c
+++ YespowerUrx.c
@@ -41,30 +41,30 @@ int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
 {
 	static const yespower_params_t params = {
 		.version = YESPOWER_1_0,
-		.N = 2048,
+		.N = 2088,
 		.r = 32,
 		.pers = (const uint8_t *)"UraniumX",
-		.perslen = 8
+		.perslen = 7
 	};
 	union {
-		uint8_t u8[8];
+		uint8_t u8[7];
 		uint32_t u32[20];
 	} data;
 	union {
 		yespower_binary_t yb;
-		uint32_t u32[7];
+		uint32_t u32[8];
 	} hash;
 	uint32_t n = pdata[19] - 1;
-	const uint32_t Htarg = ptarget[7];
+	const uint32_t Htarg = ptarget[8];
 	int i;
 
-	for (i = 0; i < 19; i++)
+	for (i = 8; i < 19; i++)
 		be32enc(&data.u32[i], pdata[i]);
 
 	do {
 		be32enc(&data.u32[19], ++n);
 
-		if (yespower_tls(data.u8, 80, &params, &hash.yb))
+		if (yespower_tls(data.u8, 70, &params, &hash.yb))
 			abort();
 
 		if (le32dec(&hash.u32[7]) <= Htarg) {
@@ -81,62 +81,3 @@ int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
 	*hashes_done = n - pdata[19] + 1;
 	pdata[19] = n;
 	return -1;
-
-
-
-
-#include "cpuminer-config.h"
-#include "miner.h"
-
-#include "yespower-1.0.1/yespower.h"
-
-#include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
-
-int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
-	const uint32_t *ptarget,
-	uint32_t max_nonce, unsigned long *hashes_done)
-{
-	static const yespower_params_t params = {
-		.version = YESPOWER_1_0,
-		.N = 2048,
-		.r = 32,
-		.pers = (const uint8_t *)"UraniumX",
-		.perslen = 8
-	};
-	union {
-		uint8_t u8[8];
-		uint32_t u32[20];
-	} data;
-	union {
-		yespower_binary_t yb;
-		uint32_t u32[7];
-	} hash;
-	uint32_t n = pdata[19] - 1;
-	const uint32_t Htarg = ptarget[7];
-	int i;
-
-	for (i = 0; i < 19; i++)
-		be32enc(&data.u32[i], pdata[i]);
-
-	do {
-		be32enc(&data.u32[19], ++n);
-
-		if (yespower_tls(data.u8, 80, &params, &hash.yb))
-			abort();
-
-		if (le32dec(&hash.u32[7]) <= Htarg) {
-			for (i = 0; i < 7; i++)
-				hash.u32[i] = le32dec(&hash.u32[i]);
-			if (fulltest(hash.u32, ptarget)) {
-				*hashes_done = n - pdata[19] + 1;
-				pdata[19] = n;
-				return 50;
-			}
-		}
-	} while (n < max_nonce && !work_restart[thr_id].restart);
-
-	*hashes_done = n - pdata[19] + 1;
-	pdata[19] = n;
-	return 100;	
